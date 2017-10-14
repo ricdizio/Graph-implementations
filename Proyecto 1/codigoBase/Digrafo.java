b2833a8cf1089e1c98adbb23c38db9e35f890da9
Ricardo Di Zio <11-11274@usb.ve>
2017-10-14 18:17:44 -0400

    saved

--- Proyecto 1/codigoBase/Digrafo.java
+++ Proyecto 1/codigoBase/Digrafo.java
@@ -6,9 +6,39 @@ import java.util.*;
 
 public class Digrafo implements Grafo
 {
-    public Digrafo() {}
+    private int numeroDeVertices;
+    private int numeroDeLados;
+    private List<Vertice> lista_de_vertices;
+    private List<Arco> lista_de_arcos;
+
+    public Digrafo() {
+        numeroDeVertices = 0;
+        numeroDeLados = 0;
+        lista_de_vertices = new LinkedList<Vertice>();
+        lista_de_arcos = new LinkedList<Arco>();
+        
+    }
 
     public boolean cargarGrafo(String dirArchivo) {
+        // Continuar despues de implementar las funciones del grafo ya que son necesarias para terminar de cargar el grafo
+        //In in = new In(args[0]);
+        In in = new In(dirArchivo);
+        int cantidad_de_nodos = in.readInt();
+        int cantidad_de_arcos = in.readInt();
+
+        for (int i=0;i<cantidad_de_nodos;i++) {
+            String id_del_vertice = in.readString();
+            double peso_del_vertice = in.readDouble();
+            // se proceden a crear y verificar si ya fueron agregados los vertices
+        }
+
+        for (int i=0;i<cantidad_de_aristas;i++) {
+            String id_de_arco = in.readString();
+            String id_vertice_de_Salida = in.readString();
+            String id_vertice_de_Llegada = in.readString();
+            double peso_del_vertice = in.readDouble();
+            // se proceden a crear y verificar si ya fueron agregados los acos
+        }
     }
     
     public int numeroDeVertices() {
