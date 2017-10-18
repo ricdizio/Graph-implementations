d866947a486a33e5618c70835c49d62e45a64970
Ricardo Di Zio <11-11274@usb.ve>
2017-10-18 05:19:25 -0400

    GND Load file

--- Proyecto 1/codigoBase/GrafoNoDirigido.java
+++ Proyecto 1/codigoBase/GrafoNoDirigido.java
@@ -22,28 +22,34 @@ public class GrafoNoDirigido implements Grafo
 
     public boolean cargarGrafo(String dirArchivo) {
         // Continuar despues de implementar las funciones del grafo ya que son necesarias para terminar de cargar el grafo
+        try{
+            In in = new In(dirArchivo);
+            int cantidad_de_nodos = in.readInt();
+            int cantidad_de_aristas = in.readInt();
 
-        In in = new In(dirArchivo);
-        int cantidad_de_nodos = in.readInt();
-        int cantidad_de_aristas = in.readInt();
+            for (int i=0;i<cantidad_de_nodos;i++) {
+                String id_del_vertice = in.readString();
+                double peso_del_vertice = in.readDouble();
+                
+                agregarVertice(id_del_vertice, peso_del_vertice);
+            }
 
-        for (int i=0;i<cantidad_de_nodos;i++) {
-            String id_del_vertice = in.readString();
-            double peso_del_vertice = in.readDouble();
-            
-            agregarVertice(id_del_vertice, peso_del_vertice);
+            for (int i=0;i<cantidad_de_aristas;i++) {
+                String id_de_arista = in.readString();
+                String id_vertice_de_Salida = in.readString();
+                String id_vertice_de_Llegada = in.readString();
+                double peso_de_arista = in.readDouble();
+                
+                agregarArista(id_de_arista, peso_de_arista,id_vertice_de_Salida, id_vertice_de_Llegada);
+                
+            }
         }
-
-        for (int i=0;i<cantidad_de_aristas;i++) {
-            String id_de_arista = in.readString();
-            String id_vertice_de_Salida = in.readString();
-            String id_vertice_de_Llegada = in.readString();
-            double peso_de_arista = in.readDouble();
-            
-            agregarArista(id_de_arista, peso_de_arista,id_vertice_de_Salida, id_vertice_de_Llegada);
-            
+        catch(Exception e){
+            System.out.println("No se pudo cargar el archivo");
+            return false;
         }
         return true;
+    }
 
     }
     
