83d82576d9b29f7cd9010ee2177718ad518d74a2
Ricardo Di Zio <11-11274@usb.ve>
2017-10-20 20:24:12 -0400

    cliente

--- Lab 2/DeepWeb.java
+++ Lab 2/DeepWeb.java
@@ -9,18 +9,19 @@ public class DeepWeb {
   public static void main(String [] args) {
 
     String file = args[0];
-    int origen = args[1].parseInt();
+    int a = args[1].parseInt();
     String algoritmo = args[2];
 
     Digrafo g = new Digrafo();
     g.cargarGrafo(file);
     String d = new String("DFS");
     String b = new String("BFS");
-    if(algoritmo==d.String.equalsIgnoreCase()){
 
+    if(algoritmo==d.String.equalsIgnoreCase()){
+        DepthFirstSearch x = new DepthFirstSearch(g,a);
     }
     else if(algoritmo==b.String.equalsIgnoreCase()){
-
+        BreadthFirstDirectedPaths x = new BreadthFirstDirectedPaths(g,a);
     }
 
   }
