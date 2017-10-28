461b3663a940d72588627e8cf9df600b5a0c94c5
Unknown <fadasgo@gmail.com>
2017-10-28 02:38:59 -0400

    Lectura

--- Lab 6/SumaArboles.java
+++ Lab 6/SumaArboles.java
@@ -6,103 +6,89 @@ import java.util.*;
 import java.io.*;
 
 public class SumaArboles{
-    private List<String> arregloDeLineas;
+    public List<String> arregloDeLineas;
     //private In in;
-    private int cuentaLineas;
-    private String words[];
+    public int cuentaLineas;
+    public String words[];
     //private String line;
   
-  public void main(String [] args) {
-        this.arregloDeLineas = new ArrayList<String>();
+  public static void main(String [] args) {
+        List<String>  arregloDeLineas = new ArrayList<String>();
         //this.in = new In(file);
-        this.cuentaLineas = 1;
+        String words[];
+        int cuentaLineas = 1;
         //Cargamos archivo
         String line;
-        String file = args[0];
+        String linee;
         Digrafo g = new Digrafo();
         //String line;
-        String words[];
+        //String words[];
         List<String> words1 = new ArrayList<String>();
-        int index = 0;
-        String acumulador = "";
-        List<Integer> acumulador1 = new ArrayList<Integer>();
+        String file = args[0];
         In in = new In(file);
-        // Extraemos los enteros
-        while(in.hasNextLine()){
-            line = in.readLine().replaceAll("\\s+","");
-            words = line.split("");
-            index = line.indexOf("(");
-            if (line.substring(0,index) != " ") {
-                words1.add(line.substring(0,index));   
-            }
-        }
-    
-        while(in.hasNextLine()){
-            line = in.readLine().replaceAll("\\s+","");
-            words = line.split("");
-            this.MetodoGrafoVariasLineas(words,in, line);
-            acumulador = "";
-            // Sacamos los enteros de la suma de los caminos
-            for(int i=0;i<words.length;i++){
-                if (words[i].equals("("))
-                {
-                    break;
-                }
-                else
-                {
-                    acumulador = acumulador + words[i];
-                }
-            }
-        }
-    }
-    
-    public void MetodoGrafoVariasLineas(String[] words, In in, String line){
+        StringBuilder builder = new StringBuilder();
+
         int contador = 0;
         int contador1 = 0;
         int count = 0;
+        int index = 0;
+        int index1 = 0;
         String tempLine;
         String tempWords[];
         String[] linea;
-        int cuentaLineas; // cuenta la cantidad de lineas consecutivas de la lectura que pertenecen al mismo grafo
 
+        // Extraemos los enteros
+        while(in.hasNextLine()){
+            line = in.readLine().replaceAll("\\s+","");
+            //words = line.split("");
+            index = line.indexOf("(");
+            if (line.substring(0,index) != " ") {
+                words1.add(line.substring(0,index)); 
+            }
+        }
+
+        for (int i=0;i<words1.size();i++) {
+            System.out.println(words1.get(i));
+            
+        }
+    
+        In iin = new In(file);
+        linee = iin.readAll().replaceAll("\\s+","");
+        words = linee.split("");
         for (int i=0; i < words.length;i++) {
+            if (contador >= 1) {
+                builder.append(words[i]);
+                //System.out.println("Esta entrando2");
+            }
             if (words[i].equals("(")) {
+                //System.out.println("Esta entrando");
+                builder.append(words[i]);
                 contador++;
             }
             if (words[i].equals(")")) {
+                //System.out.println("Esta entrando1");
                 contador1++;
             }
-            if (contador == contador1 && i+1 == words.length) {
-                if (this.cuentaLineas == 1) {
-                    this.arregloDeLineas.add(line);
-                    //return arregloDeLineas;
-                }
-                if (this.cuentaLineas > 1) {
-                    /// volver un string poner en el add
-                    this.arregloDeLineas.add(Arrays.toString(words));
-                    //return arregloDeLineas;
-                }
-            }
-            if (contador != contador1 && i+1 == words.length) {
-                //Implementar llamada recursiva para este caso
-                this.cuentaLineas++;
-                tempLine = in.readLine().replaceAll("\\s+","");
-                tempWords = tempLine.split("");
-                linea = new String[words.length+tempWords.length];
-                //Concatena ambos arreglos
-                for(int k = 0; k<words.length; k++) { 
-                    linea[k] = words[k];
-                    count++;
-                } 
-                for(int j = 0;j<tempWords.length;j++) { 
-                    linea[count+1] = tempWords[j];
-                    count++;
-                } 
-                //linea = ArrayUtils.addAll(words,tempWords);
-                this.MetodoGrafoVariasLineas(linea,in,line);
+            if (contador == contador1) {
+                //System.out.println(builder.toString());
+                arregloDeLineas.add(builder.toString());
+                contador = 0;
+                contador1 = 0;
+                //StringBuilder builder = new StringBuilder();
+                builder.setLength(0);
+            }            
+        }
 
-            }
+        //for (int i=0;i<arregloDeLineas.size();i++) {
+        //    System.out.println(arregloDeLineas.get(i));
+            
+        //}
+        //System.out.println(linee);
+        for (int i=0;i<arregloDeLineas.size();i++) {
+            System.out.println(arregloDeLineas.get(i));
+            
         }
     }
-
 }
+
+
