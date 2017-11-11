4b793727e6285a06d054e2f3808da4acb30f0b10
Ricardo Di Zio <11-11274@usb.ve>
2017-11-11 02:03:27 -0400

    commit

--- Lab7/Dijkstra.java
+++ Lab7/Dijkstra.java
@@ -1,56 +1,69 @@
 import java.lang.*;
 import java.util.*;
+import java.text.DecimalFormat;
 
 public class Dijkstra{
 	
 	public List<Vertice> listadeVertices;
 	public String caminos[];
+	public int lados[];
+	public Double costos[];
+	public DecimalFormat df;
 
 	public Dijkstra(GrafoNoDirigido G, String s)
 	{
 			//PriorityQueue <Vertice>  colaP = new PriorityQueue <Vertice> ()
 		// ARRAYsLIST METODO SORTING
+		df = new DecimalFormat("0.0#");
 		Double inf = Double.MAX_VALUE;
 		Vertice x;
 		Vertice ver;
 		List<Vertice>  cola = new ArrayList<Vertice> (); 
-		Double costos[] = new Double[G.numeroDeVertices()];
-		this.caminos = new String[G.numeroDeVertices()]; 
+		this.costos = new Double[G.numeroDeVertices()];
+		this.caminos = new String[G.numeroDeVertices()];
+		this.lados = new int[G.numeroDeVertices()]; 
+
 
 		listadeVertices = G.vertices();
 			
 		for (Vertice v: listadeVertices)
 		{
-			costos[doubleToInt(v)] = inf;
+			this.costos[doubleToInt(v)] = inf;
 			this.caminos[doubleToInt(v)] = "";
+			this.lados[doubleToInt(v)] = 0;
 			cola.add(v);
 			if ( s.equals(v.getId())) {
 				ver = v;
-				costos[doubleToInt(ver)] = 0.0;				
+				this.costos[doubleToInt(ver)] = 0.0;				
 			}
 		}
 		// Ordenamos el array list (cola de prionidad)
 		Comparator<Vertice> comp = (Vertice a, Vertice b) -> {
-    		return costos[doubleToInt(a)].compareTo(costos[doubleToInt(b)]);
+    		return this.costos[doubleToInt(a)].compareTo(this.costos[doubleToInt(b)]);
 		};
 
 		Collections.sort(cola,comp);
 
-		while (cola.size() > 0){
+		while (cola.size() > 0)
+		{
 			Collections.sort(cola,comp);
 			x = cola.remove(0);
 
-			//System.out.println(x);
-			for (Vertice v1: x.getListaDeAdyacencias()) {
-				System.out.println(costos[doubleToInt(v1)]);
-				System.out.println(costos[doubleToInt(x)]);
-				System.out.println(this.costo(x, v1));
-				System.out.println("");
-				if (costos[doubleToInt(v1)] > (costos[doubleToInt(x)] + this.costo(x, v1))) {
-					costos[doubleToInt(v1)] = costos[doubleToInt(x)] + this.costo(x, v1);
+			for (Vertice v1: x.getListaDeAdyacencias()) 
+			{
+
+				if (this.costos[doubleToInt(v1)] > (this.costos[doubleToInt(x)] + this.costo(x, v1))) 
+				{
+					this.costos[doubleToInt(v1)] = this.costos[doubleToInt(x)] + this.costo(x, v1);
+					this.lados[doubleToInt(v1)] = this.lados[doubleToInt(x)] + 1;
 					this.caminos[doubleToInt(v1)] = this.caminos[doubleToInt(x)] + "->" + x.getId();											
 				}					
 			}
+		}
+		for(int i = 0; i < G.numeroDeVertices(); i++)
+		{
+			this.caminos[i] = this.caminos[i] + "->" + i;
+			this.caminos[i] = this.caminos[i].substring(2, this.caminos[i].length());
 		}
 	}
 
@@ -74,8 +87,8 @@ public class Dijkstra{
 	public void toString(GrafoNoDirigido g)
 	{
 		for(int i = 0; i < g.numeroDeVertices(); i++)
-		{
-			System.out.println("Nodo " + i + ": " + this.caminos[i]);
+		{	
+			System.out.println("Nodo " + i + ": " + this.caminos[i]+" " + lados[i] + " lados" + " (costo "+ df.format(this.costos[i])+ ")");
 		}
 	}
 }
