762eb4ca41b5f1e6ab7e73782d679bc9e5cfdf6a
Ricardo Di Zio <11-11274@usb.ve>
2017-11-10 21:56:57 -0400

    commit

--- Lab7/Mesero.java
+++ Lab7/Mesero.java
@@ -8,36 +8,10 @@ public class Mesero{
 		String cocina = new String (args[1]);
 		int numeroDeNodos = 0;
 		GrafoNoDirigido gnd = new GrafoNoDirigido();
-<<<<<<< HEAD
 		
-=======
-		//String cocina = new String (args[1]); 
-		String idTemp;
-		String nodo1;
-		String nodo2;
-
-		//numeroDeNodos = in.readInt();
-		
-		/**for(int i = 0; i < numeroDeNodos; i++) {
-			nodoInicial = in.readInt();
-			nodoFinal = in.readInt();
-			Vertice  vertice = new Vertice(String.valueOf(i), 1.0);
-			vertice.ejeX = nodoInicial;
-			vertice.ejeY = nodoFinal;
-			gnd.agregarVertice(vertice);
-		}*/
->>>>>>> 10eea5c2fe888d50024c86df88c50b4d7ec5b3de
 		gnd.cargarGrafo(input_txt);
 
-<<<<<<< HEAD
 		String x = gnd.toString();
-=======
-		/**for(int i = 0; i < numeroDeLados;i++ ) {
-			nodo1 = in.readString();
-			nodo2 = in.readString();
-			gnd.agregarArista(String.valueOf(i), 1.0,nodo1,nodo2);
-		}*/
->>>>>>> 10eea5c2fe888d50024c86df88c50b4d7ec5b3de
 
 		System.out.println(x);
 	}
