e256afd36b7ac17dc797024351ee06e0f20314b9
Unknown <fadasgo@gmail.com>
2017-11-10 22:05:19 -0400

    Super Dijkstra

--- Lab7/Dijkstra.java
+++ Lab7/Dijkstra.java
@@ -2,31 +2,6 @@ import java.lang.*;
 import java.util.*;
 
 public class Dijkstra{
-<<<<<<< HEAD
-
-	public static void main(String[] args) 
-	{
-		Int inf = Integer.MAX_VALUE;
-
-		public Dijkstra(GrafoNoDirigido G, Vertice s){
-			PriorityQueue <Vertice>  colaP = new PriorityQueue <Vertice> ()
-			List<Vertice> listadeVertices = new LinkedList<Vertice>();
-			List<Integer> costos = new LinkedList<Integer>();
-			List<String> caminos = new LinkedList<String>();
-			listadeVertices = G.vertice();
-			for (Vertice v: listadeVertices) {
-				costos.add(listadeVertices.getIndex(v), inf);
-				caminos.add(listadeVertices.getIndex(v), null);
-				colaP.add(v);
-			}
-			costos.add(listadeVertices.getIndex(s), 0);
-
-			// usamos la cola
-
-			while (colaP.size() > 0){
-				
-								
-=======
 	
 	public List<Vertice> listadeVertices;
 
@@ -58,24 +33,10 @@ public class Dijkstra{
 					costos[doubleToInt(v1)] = costos[doubleToInt(x)] + costo(x, v1);
 					caminos[doubleToInt(v1)] = caminos[doubleToInt(v1)] + "->" + x.getId();											
 				}					
->>>>>>> 10eea5c2fe888d50024c86df88c50b4d7ec5b3de
 			}
 		}
 	}
 
-<<<<<<< HEAD
-		public int costo(Vertice x, Vertice y)
-		{
-			int sumX = 0;
-			int sumY = 0;
-			int distEuclid = 0;
-			sumX = Math.pow((x.ejeX - y.ejeX),2);
-			sumY = Math.pow((x.ejeY - y.ejeY),2);
-			distEuclid = Math.sqrt(sumX + sumY);
-			return distEuclid;
-
-		}
-=======
 	public int doubleToInt(Vertice v){
 		int i = (int)v.getPeso();
 		return i;
@@ -90,6 +51,5 @@ public class Dijkstra{
 		distEuclid = (int)Math.sqrt(sumX + sumY);
 		return distEuclid;
 
->>>>>>> 10eea5c2fe888d50024c86df88c50b4d7ec5b3de
 	}
 }
