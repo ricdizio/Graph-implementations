30ffdd344fa7a9354d8a1d128322dff29bf86cd4
Unknown <fadasgo@gmail.com>
2017-11-11 22:42:51 -0400

    Desagues / Makefile  actualizados

--- ProyectoII/Desagues.java
+++ ProyectoII/Desagues.java
@@ -1,7 +1,14 @@
-public class Charcos{
+public class Desagues{
 
 	public static void main(String [] args) {
-		
+
+		String input_txt = args[0];
+
+		Digrafo gd = new Digrafo();
+
+		gd.cargarGrafo(input_txt);
+		gd.toString();
+
 
 	}
 }
