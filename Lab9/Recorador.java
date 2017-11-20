ccb4fc0676790fa790c52afa8cceca6a28071b5d
Ricardo Di Zio <11-11274@usb.ve>
2017-11-19 23:29:59 -0400

    commit

--- Lab9/Recorador.java
+++ Lab9/Recorador.java
@@ -7,13 +7,10 @@ public class Recorador
 		// Vertice de partida con id cocina
 		String cocina = new String (args[1]);
 
-		int numeroDeNodos = 0;
-
 		GrafoNoDirigido gnd = new GrafoNoDirigido();
 
 		gnd.cargarGrafo(input_txt);
 
-<<<<<<< Updated upstream:Lab9/Recorador.java
 		AEstrella costo;
 
 		for( Vertice c :gnd.vertices())
@@ -24,11 +21,5 @@ public class Recorador
 				costo = new AEstrella(gnd,cocina,c);
 			}
 		}
-=======
-		//Dijkstra d = new Dijkstra(gnd,cocina);
-		AStar a = new AStar(gnd,cocina);
-
-		a.toString(gnd);
->>>>>>> Stashed changes:Lab9/Mesero.java
 	}
 }
