3d2abb09bcc6f146e01292e3a0c317f74a6eada7
Ricardo Di Zio <11-11274@usb.ve>
2017-12-06 13:35:00 -0400

    proyecto III

---
+++ Proyecto III/Charcos.java
@@ -0,0 +1,159 @@
+/**********************************************************************************************************
+ *    Compilacion:  javac Charcos.java
+ *    Ejecucion:    java  Charcos
+ *    Dependencias In.java, Out.java,Lado.java, Arco.java, Grafo.java, Digrafo.java, Tarjan.java
+ *    
+ *    El makefile compila todo el proyecto siguiendo el orden de dependencias
+ *    
+ *    ProyectoII
+ *
+ *    @author  Ricardo Di Zio 11-11274
+ *    @author  Fabio Suarez   12-10578
+ *    @version 1.0
+ *    @since   2017-14-11
+ *
+ * 
+ *************************************************************************************************************/
+import java.util.Stack;
+import java.util.HashSet;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class Charcos
+{
+	public int len;
+	public LinkedList<Vertice> calcCharcos(Digrafo gd)
+	{
+		this.len = 0;
+		Tarjan x = new Tarjan(gd);
+		LinkedList<Vertice> returnList = new LinkedList<Vertice>();
+		List<List<Vertice>> conjunto;
+		conjunto = x.getComponentes();
+		Stack<List<Vertice>> charco = new Stack<List<Vertice>>();
+
+
+		for(List<Vertice> element : conjunto)
+		{
+			// Element es un conjunto de vertices las cuales representan una componete conexa
+			boolean candidatoComponente = true; //element es candidato a charco?
+			for(Vertice s : element)
+			{
+   				//Si el vertice pertece a un borde descartamos la componente conexa 
+   				if(s.esquina == true) 
+   				{
+   					candidatoComponente = false;
+   					break;
+   				}
+   				//Caso contrario
+   				else
+   				{
+   					boolean candidato = true;
+   					for(Vertice d : s.getListaDeSucesores())
+   					{
+   						if(d.getPeso() < s.getPeso())
+   						{
+   							candidato = false;
+   							break;
+   						}
+   					}
+   					if(!candidato)
+   					{
+   						candidatoComponente = false;
+   						break;
+   					}
+   				}
+   			}
+			if(candidatoComponente)
+   			{
+   				charco.push(element);
+   			}
+		}
+
+		while(!charco.empty())	
+		{
+			List<Vertice> u = charco.pop();
+			for(Vertice i : u)
+			{
+				i.charco = true;
+				returnList.add(i);
+				this.len++;
+			}
+		}
+		return returnList;
+	}
+	/**  
+     * @param Entrada un digrafo
+     * @param Entrada int cantidad, el cual representa la suma hasta ese momento de los calculos
+   	 * de los metros cubicos
+     * @return retorna la cantidad de la suma actualizada para la siguiente llamada recursiva
+     * 
+     * Tiempo: O(V al cubo +E)
+     */
+	public int calcCharcosRec(Digrafo gd, int cantidad)
+	{
+		LinkedList<Vertice> lista;
+		lista = this.calcCharcos(gd);
+		do
+		{
+			for(Vertice s : lista)
+			{
+
+				boolean sumar = true;
+				for(Vertice p : s.getListaDePredecesores())
+				{
+					if(p.getPeso() <= s.getPeso() && !s.getListaDePredecesores().contains(p))
+					{
+						sumar = false;
+						break;
+					}
+					else if(p.esquina == true && p.getPeso() == s.getPeso())
+					{
+						sumar = false;
+						break;
+					}			
+			    }
+			    if(sumar)
+			    {
+			    	s.peso = s.peso + 1;
+			    	cantidad++;
+
+			    	Iterator<Vertice> iteradorV = s.getListaDePredecesores().iterator();
+
+					while(iteradorV.hasNext())
+					{
+						Vertice y = iteradorV.next();
+						if(s.getPeso() == y.getPeso() && !(lista.contains(s) && lista.contains(y)))
+			    		{
+			    			int numero = Integer.valueOf((gd.numeroDeLados()));
+			    			numero++;
+			    			gd.agregarArco(String.valueOf(numero),0.0,s.getId(),y.getId());
+			    		}
+					}
+			    }
+			}
+			//Reseteamos Vertices para el tarjan
+			for (Vertice e : gd.vertices())
+			{
+				e.reset();
+			}
+			lista = this.calcCharcos(gd);
+			
+		}while(lista.size() != 0);
+		return cantidad;
+	}
+
+	public static void main(String [] args) 
+	{
+		String input_txt = args[0];
+
+		Digrafo gd = new Digrafo();
+
+		gd.cargarGrafo(input_txt);
+
+		Charcos r = new Charcos();
+		int cantidad = r.calcCharcosRec(gd,0);
+		System.out.println("El volumen es igual a " + cantidad + " metros cubicos"+"\n");
+
+	}
+}
---
+++ Proyecto III/Vertice.java
@@ -0,0 +1,146 @@
+/**********************************************************************************************************
+ *  Compilacion:  javac Vertice.java
+ *  Ejecucion:    java Vertice
+    Dependencias: ninguna
+ *
+ *  Clase Vertice que se utiliza para poder compilar y correr Arista.java, Arco.java,GrafoNoDirigido.java,  
+ *  Digrafo.java y ClienteGrafo.java 
+ *  los cuales forman  parte de la implementacion de la estructura de datos grafo no dirigido y dirigido mediante  
+ *  el uso de las estructuras de datos de hashMap y listas enlazadas. 
+ *    
+ *  @author  Ricardo Di Zio 11-11274
+ *  @author  Fabio Suarez   12-10578
+ *  @version 1.0
+ *  @since   2017-10-19
+ * 
+ *************************************************************************************************************/
+/* 
+   * Clase constructora que inicializa al Vertice con atributo id el cual representa 
+   * la identificacion del Vertice de tipo String y el atributo peso el cual es de tipo double 
+   * y representa el valor del vertice, a su vez tambien inicializa las listas de vertices de adyacencias
+   * sucesores y predecesores, e inicializa la lista de lados la cual representa las incidencias para cada vertice. 
+   *
+   */
+
+import java.util.*;
+
+public class Vertice
+{
+  private String id;
+  public double peso;
+  public List<Vertice> adyacencias;
+  public List<Vertice> sucesores;
+  public List<Vertice> predecesores;
+  public List<Lado> incidencias;
+
+  //Atributos targan
+  public int index;
+  public int lowLink;
+  public boolean onStack;
+  public boolean esquina;
+  public boolean charco;
+
+
+  /**  
+     * @param tipo string el cual representa la identificacion del vertice
+     * @param tipo double el cual representa el valor del Vertice  
+     *
+     */
+  
+  public Vertice(String id, double peso) 
+  {
+    this.esquina = false;
+    this.charco = false;
+    this.index = -1;
+    this.onStack = false;
+    this.id = id;
+    this.peso = peso;
+    this.adyacencias = new LinkedList<Vertice>();
+    this.sucesores = new LinkedList<Vertice>();
+    this.predecesores = new LinkedList<Vertice>();
+    this.incidencias = new LinkedList<Lado>();
+
+    List<Vertice> adyacencias = new LinkedList<Vertice>();
+    List<Vertice> sucesores = new LinkedList<Vertice>();
+    List<Vertice> predecesores = new LinkedList<Vertice>();
+    List<Lado> incidencias = new LinkedList<Lado>();
+  }
+
+  /**  
+    * @param No posee parametro de entrada 
+    * @return Devuelve el valor del atributo peso del objeto vertice el cual es del tipo double 
+    *
+    */
+
+  public double getPeso() {
+    return this.peso;
+  }
+
+  /**  
+    * @param No posee parametro de entrada 
+    * @return Devuelve el valor del atributo id del objeto vertice el cual es del tipo string 
+    *
+    */
+
+  public String getId() {
+    return this.id;
+  }
+
+  /**  
+    * @param No posee parametro de entrada 
+    * @return Devuelve los datos del Vertice en una cadena de string
+    *
+    */
+
+  public String toString() { 
+    return "El Vertice tiene el id: " + this.id + " y peso: " + this.peso; 
+  }
+
+  /**  
+    * @param No posee parametro de entrada 
+    * @return una lista enlazada de objetos de tipo vertice, la cual representa una lista de adyacencia 
+    *
+    */
+
+  public List<Vertice> getListaDeAdyacencias(){
+    return this.adyacencias;
+  }
+
+  /**  
+    * @param No posee parametro de entrada 
+    * @return una lista enlazada de objetos de tipo lado, la cual representa una lista de incidencias 
+    *
+    */
+
+  public List<Lado> getListaDeIncidencias(){
+    return this.incidencias;
+  }
+
+  /**  
+    * @param No posee parametro de entrada 
+    * @return una lista enlazada de objetos de tipo vertice, la cual representa una lista de vertices predecesores 
+    *
+    */
+
+  public List<Vertice> getListaDePredecesores(){
+    return this.predecesores;
+  }
+
+  /**  
+    * @param No posee parametro de entrada 
+    * @return una lista enlazada de objetos de tipo vertice, la cual representa una lista de vertices sucesores 
+    *
+    */
+
+  public List<Vertice> getListaDeSucesores(){
+    return this.sucesores;
+  }
+
+  public void reset()
+  {
+    //this.esquina = false;
+    this.charco = false;
+    this.index = -1;
+    this.onStack = false;
+  }
+}
